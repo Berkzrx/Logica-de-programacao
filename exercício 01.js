9812599680e64286e1f7a1168fa16d8d5e007ed3
Berkzrx <patricio.grange49@gmail.com>
2022-06-14 02:17:18 -0300

    initial commit

---
+++ exercício 01.js
@@ -0,0 +1,8 @@
+// Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+// multiplicação e divisão desses valores.
+
+const operacoes = (a,b) => {
+    console.log (a+b, a-b, a*b, a/b)
+};
+
+operacoes(5,7);
