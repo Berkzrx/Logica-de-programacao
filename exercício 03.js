bf7c6480ddbf7d4c86f5b4bc48c099c30fe37103
Berkzrx <patricio.grange49@gmail.com>
2022-06-14 02:18:34 -0300

    initial commit

---
+++ exercício 03.js
@@ -0,0 +1,8 @@
+
+// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+const exponenciacao = (a,b) => {
+    console.log(Math.pow(a,b));
+};
+
+exponenciacao(5,2);
