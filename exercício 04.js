0ea5786ae2006de707a5e79a352c1b372509fa7a
Berkzrx <patricio.grange49@gmail.com>
2022-06-14 02:19:01 -0300

    initial commit

---
+++ exercício 04.js
@@ -0,0 +1,11 @@
+
+// Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+// e o resto da divisão destes dois valores.
+
+const divisaoEResto = (a,b) => {
+    console.log(a / b);
+    console.log(a % b);
+};
+
+divisaoEResto(2,5);
+divisaoEResto(7,3);
